e7a68c91502ac95274785d51b3474ad6ecf8bd92
Guilherme Santos <sntguilherme@gmail.com>
2016-04-18 20:55:16 -0300

    template working...

--- src/angular-preload-style.js
+++ src/angular-preload-style.js
@@ -1,21 +1,12 @@
 'use strict';
 
-angular.module('angular.preload.style', [])
+angular.module('angular.preload.style', ['templates'])
 
     .constant('preloadStyleConfiguration', {
         templateUrl: 'loading.html',
         scriptBaseUrl: '',
         cssFile: ''
     })
-
-    .run(['preloadStyleConfiguration', '$templateCache', '$templateRequest',
-        function (preloadStyleConfiguration, $templateCache, $templateRequest) {
-
-        $templateRequest(preloadStyleConfiguration.templateUrl).then(function (template) {
-            $templateCache.put(preloadStyleConfiguration.templateUrl, template)
-        })
-
-    }])
 
     .directive('angularPreloadStyle', ['preloadStyleConfiguration', function (preloadStyleConfiguration) {
         return {
